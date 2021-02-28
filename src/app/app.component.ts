cd5a9cedaabd807e32a2e7eb2720e6da86971179
Hikaruweng <hikaruweng@Hikaruwengs-MacBook-Pro.local>
2021-02-28 18:15:14 +0700

    finish logic, waiting for cosmetic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'part4-homework2';
+export class AppComponent implements OnInit {
+  currencyList: string[];
+  sourceCurrency: number;
+  targetCurrency: number;
+  sourceNumber: string;
+  targetNumber: string;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.currencyList = [];
+    this.httpClient
+      .get('https://api.exchangeratesapi.io/latest')
+      .subscribe((res) => {
+        this.currencyList = res['rates'];
+      });
+  }
+
+  currentChanged() {
+    this.reset();
+  }
+
+  reset() {
+    this.targetNumber = '';
+  }
+
+  calculate() {
+    this.reset();
+    this.targetNumber = (
+      (parseFloat(this.sourceNumber) * this.targetCurrency) /
+      this.sourceCurrency
+    ).toFixed(2);
+    // console.log(JSON.stringify(this.sourceCurrency));
+  }
 }
